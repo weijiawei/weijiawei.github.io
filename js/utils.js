618606a39f413102105c23faaad842eff050096d
weijiawei <1467083166@qq.com>
2021-01-04 14:15:32 +0800

    Site updated: 2021-01-04 14:15:32

--- js/utils.js
+++ js/utils.js
@@ -236,7 +236,9 @@ NexT.utils = {
       // TOC item animation navigate.
       link.addEventListener('click', event => {
         event.preventDefault();
-        var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
+        //TODO fix bug 此处解析不解码，中文标题会出BUG
+        // var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
+        var target = document.getElementById(decodeURI(event.currentTarget.getAttribute('href')).replace('#', ''));
         var offset = target.getBoundingClientRect().top + window.scrollY;
         window.anime({
           targets  : document.scrollingElement,
